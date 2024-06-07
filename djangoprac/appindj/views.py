1760b774480232c260ec5cfa6e20d70d89aad0e9
harshiempire <alleharshith@gmail.com>
2024-06-07 23:23:14 +0530

    solved the issue of adding the app to installed apps for templates to route

--- djangoprac/appindj/views.py
+++ djangoprac/appindj/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def app_in_dj(request):
-    return render(request,"appindj/app_in_dj.html")
+    return render(request,'appindj/app_in_dj.html')
